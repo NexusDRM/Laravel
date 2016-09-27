8e9ad8fe032c155ae01eac3e931e98866e713e65
Roy <rechase84@gmail.com>
2016-09-27 17:31:16 -0600

    ..

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -23,8 +23,8 @@ class TransactionsController extends Controller
 
   public function getToken()
   {
-    $cse = array("MIIBCgKCAQEAyDq6BZxgtNLrXSuLW82Atwd839+iKqVEfM3lMzyD/F6oQaJ3zXruMdBUld1sbvLl56xhzwCPx1mL+0c0NcQMIQViSWXXP8/rP23zPZGsuNZXxRwGaOUG205QhpseZz+oli0Fgrj91F0vw2ftO/6TOOWfrEhHxPjThiSRF34OCnAuo/dppPmU94X30U4KK715Mol6DCusXFWgAlGVAuoZUjJ03SwNMMXh4KLyp46XBe6/nfWF6nSkkxIO5YCoOWBDQfY3HYvUlxu6OFRu7jk8N0stMc5/omQ44Gi8dW+FpUMkJ+c3vDdUykaR21n/xAtNKVNiHeriSG7w3Q9f88r/LQIDAQAB");
-    $clientToken = Braintree_ClientToken::generate($cse);
+    // $cse = array("MIIBCgKCAQEAyDq6BZxgtNLrXSuLW82Atwd839+iKqVEfM3lMzyD/F6oQaJ3zXruMdBUld1sbvLl56xhzwCPx1mL+0c0NcQMIQViSWXXP8/rP23zPZGsuNZXxRwGaOUG205QhpseZz+oli0Fgrj91F0vw2ftO/6TOOWfrEhHxPjThiSRF34OCnAuo/dppPmU94X30U4KK715Mol6DCusXFWgAlGVAuoZUjJ03SwNMMXh4KLyp46XBe6/nfWF6nSkkxIO5YCoOWBDQfY3HYvUlxu6OFRu7jk8N0stMc5/omQ44Gi8dW+FpUMkJ+c3vDdUykaR21n/xAtNKVNiHeriSG7w3Q9f88r/LQIDAQAB");
+    $clientToken = Braintree_ClientToken::generate();
 
     return response()->json([
       'clientToken'=>$clientToken
